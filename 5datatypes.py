27dfd4892422f4509af8f02de5459a377398e0f1
sami <samidev21@gmail.com>
2025-05-01 19:56:39 +0600

    different kinds data types

---
+++ 5datatypes.py
@@ -0,0 +1,21 @@
+#Different types of datatypes
+
+text_type_data="This is a text data type"
+print(text_type_data)
+
+#To know the datatype of the variables we will use the syntax given below
+
+print(type(text_type_data))
+
+# complex datatype  use 
+a = 2+3j      #Here is real part and imaginary part
+b = 4-2j      #Imaginary part will attached with j
+print(a+b)    #result is (6+1j)
+print(a-b)    #result is (-2+5j)
+
+#using bool data type
+
+x=33
+y=99
+print(x>y)   #result is False
+print(x<y)   #result is true
