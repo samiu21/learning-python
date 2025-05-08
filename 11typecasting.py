597374b592a892c03f8c126a4ab71736b4828472
sami <samidev21@gmail.com>
2025-05-08 15:10:34 +0600

    Type casting of Data types

---
+++ 11typecasting.py
@@ -0,0 +1,13 @@
+a="3434343"
+print(a)                  #output same of a and str(a)
+print(str(a))
+print(float(a))
+
+
+b="Hello World"
+print(b)
+
+print(bool(0))             #output = False
+print(bool(1))             #output = True
+print(bool(""))            #output = False
+print(bool("hello"))       #outpur = True
