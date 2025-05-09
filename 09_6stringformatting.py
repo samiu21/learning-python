2c162c0b65c00e175ebcffeba0a14e2518eb6552
sami <samidev21@gmail.com>
2025-05-09 15:55:07 +0600

    delete6

--- 09_6stringformatting.py
+++
@@ -1,9 +0,0 @@
-num1=10
-num2=30
-name="samiul islam sami"
-roll= "10"
-print("Addition of the number is ",num1+num2)    # this two will show same result
-print(f"Addition of the number is {num1+num2}")  
-
-print("MY name is " +name+ " and my roll is "+roll)  # this two will show same result
-print(f"My name is {name} and my roll is {roll}")
