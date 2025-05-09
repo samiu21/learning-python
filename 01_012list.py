1b316ee9805b309fedb48b5feaf04a7be00a13c5
sami <samidev21@gmail.com>
2025-05-09 15:26:38 +0600

    Reaname1

---
+++ 01_012list.py
@@ -0,0 +1,6 @@
+list1=[1,22,33,22]                   #mutable means we can change the value of the list
+print(list1)
+list2=["samiul","Porag","Amit","Mohabbat"]
+print(list2)
+list3=[True,False,True,False,False]
+print(list3)
---
+++ 12_9swappingtwovalues.py
@@ -0,0 +1,5 @@
+a=50
+b=100
+print(f"Before swapping the value of a is {a} and b is {b}")
+a,b=b,a
+print(f"After swappint the value of a is {a} and b is {b}")
