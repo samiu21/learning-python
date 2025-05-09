473227824d7cd728a6a1403dfd69b04e2d30c8d5
sami <samidev21@gmail.com>
2025-05-09 15:31:10 +0600

    Single and multiline comment together

--- 003_single&multilinecomment.py
+++ 003_single&multilinecomment.py
@@ -1,16 +1,22 @@
 #1.This is a single line comment
+
+
 """
 2.This is a multline comment
 we  can use it in two ways .
 Another one is given below .
 """
+
+
 '''
 3.This is the another way of using multiline comment.
 we can also use it as single line comment.
 '''
+
+
 """4.We can use multiline comment as a single line comment"""
 
 
-print("Number one is single line comment")
+print("Number one(1) is single line comment")
 print("Number 2 and 3 is multiline comment. ")
 print("We can use multiline comment as a single line comment")
