c517b4db3eea49c2f8d5c02a93059cd5318a1e0a
sami <samidev21@gmail.com>
2025-05-12 19:55:20 +0600

    remove agin in the list

---
+++ 016_removemoreitem.py
@@ -0,0 +1,14 @@
+fruits=["apple","Banana","orange","Jackfruit","pineapple"]
+del fruits[1:4]   #index 1 to 3 will remove
+print(fruits)
+
+fruits = ["apple", "banana", "orange", "jackfruit", "pineapple"]
+to_remove = ["banana", "jackfruit"]
+
+for item in to_remove:
+    if item in fruits:
+        fruits.remove(item) 
+        print(fruits)
+
+print(fruits)
+# Output: ['apple', 'orange', 'pineapple']
