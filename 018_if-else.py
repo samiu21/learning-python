e25d59f295f5d399b11f6f67a8eb194e03dc8d19
sami <samidev21@gmail.com>
2025-07-01 12:49:43 +0600

    only if else

---
+++ 018_if-else.py
@@ -0,0 +1,12 @@
+rain=1  #if it's rain , it will be 1 , if not rain it will be 0
+if rain ==1:
+    print(" I will not go to the school")
+else:
+    print ("I will go to the school")
+
+a=5
+b=4
+if a<b:
+    print("A is less than B")
+else:
+    print("A is greater than B")
