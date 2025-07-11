0f2fb5c6574736e4250be4683041e5f1cda0f7d4
sami <samidev21@gmail.com>
2025-07-11 11:54:51 +0600

    2D transepose matrix

---
+++ 016_2d_transpose_matrix.py
@@ -0,0 +1,16 @@
+#for loop use kore 2D matrix comprehension
+
+matrix = [[1,2],[3,4],[5,6],[7,8]]
+a=[]
+for row in range(2):
+    b=[]
+    for col in matrix:
+        b.append(col[row])
+    a.append(b)
+print(a)
+
+
+#list comprehension use kore
+
+res=[[col[row] for col in matrix] for row in range(2)]
+print(res)
