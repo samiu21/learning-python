16d7d5f7f7c7031d951dc8a72a363e7d91d04c89
sami <samidev21@gmail.com>
2026-01-02 19:15:25 +0600

    loop revise

--- 017_looplist.py
+++ 017_looplist.py
@@ -1,18 +1,19 @@
-looplist=["Mango","banana","Jackfruit","Lychee","palm"]
+looplist = ["Mango", "banana", "Jackfruit", "Lychee", "palm"]
 
-for i in looplist:     #print all the item
-     print(i)
-     
-#Use the range() and len() functions to create a suitable iterable
-print("\n")
-    
-for i in range(0,len(looplist)):  #all the item will print
-    print(looplist[i])  
-    
-print("\n")   
-    
-for i in (looplist[1:4]):      #banana,jackfruil,Lychee will print index 1 to 3
+for i in looplist:     # print all the item
     print(i)
-print("\n")    
-for i in range(0,3):           #banana,jackfruit will print index 0 to 2
+
+# Use the range() and len() functions to create a suitable iterable
+print("\n")
+
+for i in range(0, len(looplist)):  # all the item will print
+    print(looplist[i])
+
+print("\n")
+
+for i in (looplist[1:4]):     # banana,jackfruil,Lychee will print index 1 to 3
+
+    print(i)
+print("\n")
+for i in range(0, 3):           # banana,jackfruit will print index 0 to 2
     print(looplist[i])
